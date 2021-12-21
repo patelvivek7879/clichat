fd0ec9059304a062c17982f8770508455fdf2889
Vivek Patel <patelvivek7879@gmail.com>
2021-12-22 01:20:24 +0530

    initial commit

---
+++ client.js
@@ -0,0 +1,49 @@
+cosnt net = require("net");
+const readline = require('readline');
+const events = require('events');
+
+function acceptInput(){
+
+}
+
+class DataModel{
+
+}
+// some lines ---
+
+
+function processAction(){
+
+}
+
+async function processLoginAction(){
+
+
+
+}
+
+function processLoginActionResponse(){
+
+
+}
+
+function processLogoutAction(){}
+function processLogoutActionResponse(){}
+
+async function processAcceptCommandAction(){
+
+}
+
+function processAcceptCommandActionResponse(){
+
+}
+
+function loggedIn(){
+
+}
+
+function usersListArrived(){
+
+}
+
+//Events ---------------
---
+++ server.js
@@ -0,0 +1,107 @@
+const net = require('net');
+const fs = require('fs');
+
+class Response{
+	constructor(){
+		this.action="";
+		this.success=false;
+		this.error=null;
+		this.result=null;
+	}
+}
+
+class DataModel{
+	constructor(){
+		this.users=[];
+		this.userID=0;
+	}
+
+getUserByUsername(username){
+	var user = this.users.find(function(user){
+		return username == user.username;
+	});
+	return user;
+}
+getLoggedInUsers(){
+	var loggedInUsers=[];
+	for(var e=0;e<users.length;e++){
+		if(this.users[e].loggedIn){
+			loggedInUsers.push(users[e]);	
+		}
+	}
+return loggedInUsers;
+}
+}
+
+var model = new DataModel();
+
+function populateDataStructure(){
+	var usersJSONString = fs.readFileSync("users.data","utf-8");
+	var users = JSON.parse(usersJSONString).users;
+	users.forEach(function(user){
+		user.loggedIn=false;
+		user.id=0;
+		model.users.push(user);
+	});
+}
+
+function processRequest(requestObject){
+	if(requestObject.action=='login'){
+		let username = requestObject.username;
+		let password = requestObject.password;
+		let user = model.getUserByUsername(username);
+		var success = false;
+	if(user){
+		if(password==user.password) success = true;
+	}
+let response = new Response();
+response.action=requestObject.action;
+response.success=success;
+if(success){
+	response.error="";
+	model.userID++;
+	requestObject.socket.userID = model.userID;
+	user.id=model.userID;
+	user.loggedIn=true;
+	response.result = {
+		"username": user.username,
+		"id": user.id,
+	};
+}
+else{
+	response.error = "Invalid username / password";
+	response.result="";
+}
+responseObject.socket.write(JSON.stringify(response));
+}
+if(requestObject.action=="logout"){}
+if(requestObject.action=="getUsers"){
+	var response = new Response();
+	response.action = requestObject.action;
+	response.result = model.getLoggedInUser();
+	requestObject.socket.write(JSON.stringify(response));
+}
+}
+
+populateDataStructure();
+var server = net.createServer(function(){
+	socket.on("data",function(data){
+		var requestObject = JSON.parse(data);
+		requestObject.socket = socket;
+		try{
+		}catch(error){
+			console.log(error);
+		}
+	});
+
+socket.on("end",function(){
+	console.log("Client closed connection");
+})
+
+socket.on("error",function(){
+	console.log('Some problem at client side');
+});
+});
+
+server.listen(6000, "localhost");
+console.log("Chat server is running on port 5500");
